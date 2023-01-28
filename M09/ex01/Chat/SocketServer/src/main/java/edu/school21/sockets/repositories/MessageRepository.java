8f91d2bcf34dd006b63a9516fcd35d2fc9e45384
Analysis Mshad <nanicku@gmail.com>
2023-01-28 17:51:28 +0300

    M09

---
+++ M09/ex01/Chat/SocketServer/src/main/java/edu/school21/sockets/repositories/MessageRepository.java
@@ -0,0 +1,5 @@
+package edu.school21.sockets.repositories;
+
+public interface MessageRepository extends CrudRepository{
+}
+
---
+++ M09/ex01/Chat/SocketServer/src/main/java/edu/school21/sockets/services/UsersService.java
@@ -0,0 +1,6 @@
+package edu.school21.sockets.services;
+public interface UsersService {
+    boolean signUp(String login, String password);
+    boolean signIn(String login);
+    boolean signIn(String login, String password);
+}
